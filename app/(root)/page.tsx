254ab18f3fc4654a0b560fb568853890665082d3
uditsingh007 <singh.udit14@gmail.com>
2023-09-05 11:53:20 +0530

    Fetch all posts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,11 +5,11 @@ import { UserButton, currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const result = await fetchPosts(1, 30);
   const user = await currentUser();
   if (!user) return null;
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
+  const result = await fetchPosts(1, 30);
   return (
     <div>
       {/* <UserButton afterSignOutUrl="/" /> */}
